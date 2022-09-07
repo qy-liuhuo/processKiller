e685336c0b8fa933ddbbeb2f055dd93f80bfea53
qy-liuhuo <2499094347@qq.com>
2022-09-07 15:48:21 +0800

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+
+# 设置端口号
+PORT = 1080
+
+result = os.popen('netstat -aon|findstr "%d"' % PORT)
+res = result.read()
+res = res.split('\n')[0]
+pid = res.split(' ')[-1]
+
+os.popen("taskkill -pid %d -f" % int(pid))
